acb5075a0ff0596a6b275c50bc7762409d1a12ac
Dixon <villandixon@yahoo.com>
2020-09-19 17:16:33 +0800

    updated app and messages

--- src/App.js
+++ src/App.js
@@ -1,45 +1,68 @@
 import { Button, FormControl, Input, InputLabel } from '@material-ui/core';
 import React, {useEffect, useState} from 'react';
 import './App.css';
+import db from './firebase';
 import Message from './Message';
+import firebase from 'firebase';
+import FlipMove from 'react-flip-move';
 
 function App() {
   const [input, setInput] = useState('');
-  const [messages, setMessages] = useState([{username:'dixv', text:'whats up'}, {username:'qazi', text:'hello'}, {username:'sonny', text:'hi there!'}]);
+  // const [messages, setMessages] = useState([{username:'dixv', message:'whats up'}, {username:'qazi', message:'hello'}, {username:'sonny', message:'hi there!'}]);
+  const [messages, setMessages] = useState([]);
   const [username, setUsername] = useState('');
   
+  useEffect(() =>{
+    db.collection('messages')
+      .orderBy('timestamp', 'desc')
+      .onSnapshot(snapshot => {
+      setMessages(snapshot.docs.map(doc => ({id: doc.id, message: doc.data()})))
+    })
+  },[]);
+
   useEffect(() => {
     setUsername(prompt('Please enter your name'));
   }, []); 
 
   const sendMessage = (event) => {
     event.preventDefault();
-    setMessages([...messages, {username: username, text: input}]);
+    //---push locally
+    //setMessages([...messages, {username: username, message: input}]);
+    db.collection('messages')
+    .add({
+          message: input, 
+          username: username, 
+          timestamp: firebase.firestore.FieldValue.serverTimestamp()
+        });
     setInput('');
   }
 
   return (
     <div className="App">
-      <h1>fb Messenger Clone</h1>
-      <h2>Welcome {username}</h2>
-
-      <form>
-        <FormControl>        
-          <div className="message__entry">
-                <InputLabel>Enter a message</InputLabel>
-                <Input onChange={event => setInput(event.target.value)} value={input}/>
-                <Button type="submit" disabled={!input} color="primary" variant="contained" onClick={sendMessage}>Send Message</Button>
-          </div>
-
-          <div className="message__card">
-            {
-              messages.map(message => (
-                <Message username={username} message={message}/>
-              ))
-            }
-          </div>
-        </FormControl>        
-      </form>
+      <div className="app__header">
+        <img src="https://facebookbrand.com/wp-content/uploads/2018/09/header-e1538151782912.png?w=100&h=100" alt=""/>
+        <h1>fb Messenger Clone</h1>
+        <h2>Welcome {username}</h2>
+      </div>
+      <div className="app__message">
+              <FlipMove>
+              {
+                messages.map(({id, message}) => (
+                  <Message key={id} username={username} message={message}/>
+                ))
+              }
+              </FlipMove>
+      </div>
+      <div className="app__input">
+        <form>
+          <FormControl>        
+                  <InputLabel className="input__label">Enter a message</InputLabel>
+                  <Input onChange={event => setInput(event.target.value)} value={input}/>
+                  <Button type="submit" disabled={!input} color="primary" variant="contained" onClick={sendMessage}>Send Message</Button>
+          </FormControl>        
+        </form>
+      </div>
+     
     </div>
   );
 }
--- src/Message.js
+++ src/Message.js
@@ -1,25 +1,24 @@
 import { Card, CardContent, Typography } from '@material-ui/core';
-import React from 'react';
+import React, {forwardRef} from 'react';
 import './Message.css';
 
-function Message({message, username}) {
+const Message= forwardRef(({message, username}, ref) => {
 	const isUser = username === message.username;
     return (
-		<div className={`message ${isUser && 'message__user'}`}>
+		<div ref={ref} className={`message ${isUser && 'message__user'}`}>
 			<Card className={isUser ? "message__userCard" : "message__guestCard"}>
 				<CardContent>
 					<Typography
-						color="White"
+						color="white"
 						variant="h5"		   
 						component="h2"
 					>   
-						{isUser ? message.text : message.username + " : " + message.text}
-						{/* {message.username} : {message.text} */}
+						{isUser ? message.message : message.username + " : " + message.message}
 					</Typography>
 				</CardContent>
 			</Card>	
 		</div>
     )
-}
+})
 
 export default Message
